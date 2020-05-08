dfcb15fd50a263599bb4c74fcb47140b42c87b4f
sralloza <sralloza@gmail.com>
2020-05-08 19:03:57 +0200

    feat: add whitelist creator

---
+++ src/whitelist.py
@@ -0,0 +1,19 @@
+"""Manages server whitelist."""
+import json
+
+from src.dataframe import get_dataframe
+
+
+def create_whitelist() -> str:
+    """Creates a minecraft server whitelist with every user present in the
+    base64 dataframe, and returns it as a str.
+
+    Returns:
+        str: whitelist as str. Must be written to a file named `whitelist.json`.
+    """
+    dataframe = get_dataframe()
+    whitelist = []
+    for uuid, player in dataframe.iterrows():
+        whitelist.append({"uuid": uuid, "name": player.username})
+
+    return json.dumps(whitelist, indent=4, ensure_ascii=False,)
