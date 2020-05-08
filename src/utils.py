58a1c9aa90ab5233af59ab5c39d361162f1493ae
sralloza <sralloza@gmail.com>
2020-05-08 19:06:29 +0200

    style: improve utils
    Add getters and setters for Memory, and remove unnecessary if checks
    in str2bool.

--- src/utils.py
+++ src/utils.py
@@ -2,22 +2,30 @@ import argparse
 
 
 class Memory:
-    parser = None
+    _parser = None
+
+    @classmethod
+    def set_parser(cls, parser):
+        cls._parser = parser
+
+    @classmethod
+    def get_parser(cls):
+        return cls._parser
 
 
-def bool2str(b):
-    return str(b).lower()
+def bool2str(boolean):
+    return str(boolean).lower()
 
 
-def str2bool(v, parser=False):
-    if isinstance(v, bool):
-        return v
-    if v.lower() in ("yes", "true", "t", "y", "1"):
+def str2bool(string, parser=False):
+    if isinstance(string, bool):
+        return string
+    if string.lower() in ("yes", "true", "t", "y", "1"):
         return True
-    elif v.lower() in ("no", "false", "f", "n", "0"):
+    if string.lower() in ("no", "false", "f", "n", "0"):
         return False
-    else:
-        if parser:
-            raise argparse.ArgumentTypeError("Boolean value expected.")
-        else:
-            raise ValueError(f"{v!r} is not a valid boolean")
+
+    if parser:
+        raise argparse.ArgumentTypeError("Boolean value expected.")
+
+    raise ValueError(f"{string!r} is not a valid boolean")
