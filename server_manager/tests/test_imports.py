616d750c3620f0b9b86a3cd5a0465447b612c5b4
sralloza <sralloza@gmail.com>
2020-05-16 12:40:52 +0200

    test: ensure every requirement is installed

---
+++ server_manager/tests/test_imports.py
@@ -0,0 +1,26 @@
+def test_import_pandas():
+    import pandas as pd
+
+
+def test_import_colorama():
+    from colorama import Fore
+
+
+def test_import_nbtlib():
+    from nbtlib import File
+
+
+def test_import_numpy():
+    import numpy as np
+
+
+def test_import_pyperclip():
+    from pyperclip import copy, paste
+
+
+def test_import_requests():
+    import requests
+
+
+def test_import_xlrd():
+    import xlrd
