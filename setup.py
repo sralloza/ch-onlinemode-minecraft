1f8af19b71a1a98db3694387f835707b09caad87
sralloza <sralloza@gmail.com>
2020-05-23 16:36:45 +0200

    fix: include requirements in setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
+from pathlib import Path
+
 from setuptools import find_packages, setup
 
-setup(name="server-manager", version="1.0", packages=find_packages())
+requirements = Path(__file__).with_name("requirements.txt").read_text().splitlines()
+
+setup(
+    name="server-manager",
+    version="1.0",
+    packages=find_packages(),
+    install_requires=requirements,
+)
