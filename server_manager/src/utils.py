3d4ca86fbfc3419cd57730b61a40f86380e8f0b7
sralloza <sralloza@gmail.com>
2020-05-23 16:29:10 +0200

    docs: add docs for src.utils

--- server_manager/src/utils.py
+++ server_manager/src/utils.py
@@ -1,7 +1,10 @@
+"""Useful functions to all the package."""
+
 import argparse
 
 
 class Memory:
+    """Class to store the parser."""
     _parser = None
 
     @classmethod
@@ -13,11 +16,45 @@ class Memory:
         return cls._parser
 
 
-def bool2str(boolean):
+def bool2str(boolean: bool):
+    """Returns a bool as a string (in lowercase, as json).
+
+    Args:
+        boolean (bool): input bool.
+
+    Returns:
+        str: string representation of the input bool.
+    """
+
     return str(boolean).lower()
 
 
-def str2bool(string, parser=False):
+def str2bool(string: str, parser=False):
+    """Transforms a string to bool.
+
+    Usage:
+        >>> str2bool("True")
+        True
+        >>> str2bool("False")
+        False
+        >>> str2bool("Invalid", False)
+        ValueError("'Invalid' is not a valid boolean")
+
+
+    Args:
+        string (str): input string.
+        parser (bool, optional): If True, in case of error it will raise
+            argparse.ArgumentTypeError instead of ValueError. If it is used
+            inside parse_args, it whould be True. Defaults to False.
+
+    Raises:
+        argparse.ArgumentTypeError: if the string is not valid and parser is True.
+        ValueError: if the string is not valid and parser is False.
+
+    Returns:
+        bool: boolean value of the string.
+    """
+
     if isinstance(string, bool):
         return string
     if string.lower() in ("yes", "true", "t", "y", "1"):
