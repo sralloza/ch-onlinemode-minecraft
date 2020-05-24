468de3f35529c731a70c9f1df4d81daf9e45b282
sralloza <sralloza@gmail.com>
2020-05-24 12:18:57 +0200

    test: cover src.files

---
+++ server_manager/tests/test_src/test_files.py
@@ -0,0 +1,341 @@
+from copy import deepcopy
+from pathlib import Path
+from unittest import mock
+
+import pytest
+
+from server_manager.src.exceptions import InvalidFileError
+from server_manager.src.files import (
+    AdvancementsFile,
+    File,
+    MetaFile,
+    PlayerDataFile,
+    StatsFile,
+)
+
+
+@pytest.fixture(scope="class", autouse=True)
+def reset_file_metaclass_attrs():
+    for key in File.memory:
+        File.memory[key] = []
+
+    memory = File.memory.copy()
+    subtypes = File.subtypes.copy()
+
+    yield
+
+    File.memory = memory
+    File.subtypes = subtypes
+
+
+class TestMetaFile:
+    def test_work(self):
+        # Meta.__init__ (declaring base class)
+        class Base(metaclass=MetaFile):
+            def __init__(self, number):
+                self.number = number
+
+            def __repr__(self):
+                return f"{self.__class__.__name__}({self.number!r})"
+
+        assert hasattr(Base, "memory")
+        assert hasattr(Base, "subtypes")
+
+        class Child1(Base):
+            pass
+
+        class Child2(Base):
+            pass
+
+        assert Base.memory == {"child1": [], "child2": []}
+        assert Base.subtypes == {"child1": Child1, "child2": Child2}
+
+        # Meta.__call__ (instantiating child classes)
+        c11 = Child1(11)
+        c12 = Child1(12)
+        c13 = Child1(13)
+        c21 = Child2(21)
+        c22 = Child2(22)
+
+        assert Base.memory == {"child1": [c11, c12, c13], "child2": [c21, c22]}
+        assert Base.subtypes == {"child1": Child1, "child2": Child2}
+
+        # Some mix
+        class Child3(Base):
+            pass
+
+        c31 = Child3(31)
+        assert Base.memory == {
+            "child1": [c11, c12, c13],
+            "child2": [c21, c22],
+            "child3": [c31],
+        }
+        assert Base.subtypes == {"child1": Child1, "child2": Child2, "child3": Child3}
+
+
+class TestFile:
+    @classmethod
+    def setup_class(cls):
+        # class Base:
+        #     def __init__(self, path):
+        #         self.path = path
+
+        #     def __repr__(self):
+        #         return f"{self.__class__.__name__}({self.path!r})"
+
+        class TypeA(File):
+            pass
+
+        class TypeB(File):
+            pass
+
+        class TypeC(File):
+            pass
+
+        cls.subtypes = {"typea": TypeA, "typeb": TypeB, "typec": TypeC}
+        cls.memory = {"typea": [], "typeb": [], "typec": []}
+
+        # File.subtypes = cls.subtypes
+
+        # cls.Base = Base
+        cls.TypeA = TypeA
+        cls.TypeB = TypeB
+        cls.TypeC = TypeC
+
+    @pytest.fixture(autouse=True)
+    def reset_memory(self):
+        File.subtypes = deepcopy(self.subtypes)
+        File.memory = deepcopy(self.memory)
+        yield
+
+    @pytest.fixture
+    def file_creator(self):
+        def wrapped(path):
+            file = object.__new__(File)
+            file.__init__(path=path)
+            return file
+
+        yield wrapped
+
+    def test_uuid_pattern(self):
+        test = lambda x: File.uuid_pattern.search(x)
+
+        assert test("00000000-0000-0000-0000-000000000000")
+        assert test("550e8400-e29b-41d4-a716-446655440000")
+        assert test("00000000-0000-0000-C000-000000000046")
+        assert test("01234567-89ab-cdef-0123-456789abcdef")
+        assert not test("xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx")
+        assert not test("invalid-uuid")
+
+    def test_init(self):
+        # Not identified
+        test_file = File("path")
+        assert test_file is None
+        assert File.memory == {"typea": [], "typeb": [], "typec": []}
+
+        # Identify TypeA file
+        f1 = File("/world/typea/00000000-0000-0000-0000-000000000000.json")
+        assert isinstance(f1, self.TypeA)
+        assert File.memory == {"typea": [f1], "typeb": [], "typec": []}
+
+        # Identify TypeB file
+        f2 = File("/world/typeb/00000000-0000-0000-0000-000000000000.json")
+        assert isinstance(f2, self.TypeB)
+        assert File.memory == {"typea": [f1], "typeb": [f2], "typec": []}
+
+        # Identify TypeC file
+        f3 = File("/world/typec/00000000-0000-0000-0000-000000000000.json")
+        assert isinstance(f3, self.TypeC)
+        assert File.memory == {"typea": [f1], "typeb": [f2], "typec": [f3]}
+
+        # Identify another TypeA file
+        f4 = File("/world/typea/01234567-89ab-cdef-0123-456789abcdef.json")
+        assert isinstance(f1, self.TypeA)
+        assert File.memory == {"typea": [f1, f4], "typeb": [f2], "typec": [f3]}
+
+    def test_attributes(self, file_creator):
+        # Build object manually
+        test_file = file_creator("path")
+
+        assert hasattr(test_file, "path")
+        assert test_file.path == Path("path")
+
+        assert vars(test_file) == {"path": Path("path")}
+
+    def test_uuid(self, file_creator):
+        def test(path):
+            return file_creator(path).uuid
+
+        def not_uuid(path):
+            with pytest.raises(InvalidFileError):
+                file_creator(path).uuid
+            return True
+
+        assert test("path/file-00000000-0000-0000-0000-000000000000.json")
+        assert test("path/file-550e8400-e29b-41d4-a716-446655440000.dat")
+        assert test("path/file-00000000-0000-0000-C000-000000000046.xml")
+
+        assert not_uuid("/var/invalid/xxxxxxxx-xxxx-Mxxx-Nxxx-xxxxxxxxxxxx.invalid")
+        assert not_uuid("/home/user/test/invalid-uuid.png")
+
+    def test_as_posix(self, file_creator):
+        file = file_creator("/path/to/file.ext")
+        assert file.as_posix() == "/path/to/file.ext"
+
+    @mock.patch("pathlib.Path.rename")
+    def test_change_uuid(self, rename_m, file_creator, caplog):
+        file = file_creator("file_type/00000000-0000-0000-C000-000000000046.json")
+        new_uuid = "550e8400-e29b-41d4-a716-446655440000"
+        file.change_uuid(new_uuid)
+
+        expected_path = Path("file_type/550e8400-e29b-41d4-a716-446655440000.json")
+        rename_m.assert_called_with(expected_path)
+
+        # In this case, 'foo' shouldn't be there, so File.change_uuid will remove it.
+        file = file_creator("file_type/foo-00000000-0000-0000-C000-000000000046.json")
+        new_uuid = "550e8400-e29b-41d4-a716-446655440000"
+        file.change_uuid(new_uuid)
+
+        expected_path = Path("file_type/550e8400-e29b-41d4-a716-446655440000.json")
+        rename_m.assert_called_with(expected_path)
+
+    def test_identify(self):
+        assert File.memory == self.memory
+        assert File.subtypes == self.subtypes
+
+        a1 = File.identify("/file/typea/1")
+        a2 = File.identify("/file/typea/2")
+        a3 = File.identify("/file/typea/3")
+        b1 = File.identify("/file/typeb/1")
+        b2 = File.identify("/file/typeb/2")
+        c1 = File.identify("/file/typec/1")
+        invalid = File.identify("invalid")
+
+        assert isinstance(a1, self.TypeA)
+        assert a1.path == Path("/file/typea/1")
+
+        assert isinstance(a2, self.TypeA)
+        assert a2.path == Path("/file/typea/2")
+
+        assert isinstance(a3, self.TypeA)
+        assert a3.path == Path("/file/typea/3")
+
+        assert isinstance(b1, self.TypeB)
+        assert b1.path == Path("/file/typeb/1")
+
+        assert isinstance(b2, self.TypeB)
+        assert b2.path == Path("/file/typeb/2")
+
+        assert isinstance(c1, self.TypeC)
+        assert c1.path == Path("/file/typec/1")
+
+        assert invalid is None
+        assert File.memory == {"typea": [a1, a2, a3], "typeb": [b1, b2], "typec": [c1]}
+
+    def test_identify_2(self):
+        assert File.memory == self.memory
+        assert File.subtypes == self.subtypes
+
+        # Not identified
+        test_file = File.identify("path")
+        assert test_file is None
+        assert File.memory == {"typea": [], "typeb": [], "typec": []}
+
+        # Identify TypeA file
+        f1 = File.identify("/world/typea/00000000-0000-0000-0000-000000000000.json")
+        assert isinstance(f1, self.TypeA)
+        assert File.memory == {"typea": [f1], "typeb": [], "typec": []}
+
+        # Identify TypeB file
+        f2 = File.identify("/world/typeb/00000000-0000-0000-0000-000000000000.json")
+        assert isinstance(f2, self.TypeB)
+        assert File.memory == {"typea": [f1], "typeb": [f2], "typec": []}
+
+        # Identify TypeC file
+        f3 = File.identify("/world/typec/00000000-0000-0000-0000-000000000000.json")
+        assert isinstance(f3, self.TypeC)
+        assert File.memory == {"typea": [f1], "typeb": [f2], "typec": [f3]}
+
+        # Identify another TypeA file
+        f4 = File.identify("/world/typea/01234567-89ab-cdef-0123-456789abcdef.json")
+        assert isinstance(f1, self.TypeA)
+        assert File.memory == {"typea": [f1, f4], "typeb": [f2], "typec": [f3]}
+
+    @mock.patch("os.walk")
+    def test_gen_files(self, walk_m):
+        walk = (
+            ("./", ["typea", "typeb", "typec", "hidden"], []),
+            (
+                "./typea",
+                [],
+                [
+                    "00000000-0000-0000-0000-0000000000a1.json",
+                    "00000000-0000-0000-0000-0000000000a2.json",
+                    "00000000-0000-0000-0000-0000000000a3.json",
+                ],
+            ),
+            (
+                "./typeb",
+                [],
+                [
+                    "00000000-0000-0000-0000-0000000000b1.json",
+                    "00000000-0000-0000-0000-0000000000b2.json",
+                ],
+            ),
+            ("./hidden", ["typec"], []),
+            ("./hidden/typec", [], ["00000000-0000-0000-0000-0000000000c1.json"]),
+        )
+        walk_m.return_value = walk
+        files = File.gen_files(Path("./"))
+
+        assert files is None
+        typea = File.memory["typea"]
+        typeb = File.memory["typeb"]
+        typec = File.memory["typec"]
+
+        assert len(typea) == 3
+        assert len(typeb) == 2
+        assert len(typec) == 1
+
+        assert typea[0].path == Path("typea/00000000-0000-0000-0000-0000000000a1.json")
+        assert typea[1].path == Path("typea/00000000-0000-0000-0000-0000000000a2.json")
+        assert typea[2].path == Path("typea/00000000-0000-0000-0000-0000000000a3.json")
+
+        assert typeb[0].path == Path("typeb/00000000-0000-0000-0000-0000000000b1.json")
+        assert typeb[1].path == Path("typeb/00000000-0000-0000-0000-0000000000b2.json")
+
+        assert typec[0].path == Path(
+            "hidden/typec/00000000-0000-0000-0000-0000000000c1.json"
+        )
+
+    def test_repr(self, file_creator):
+        file = file_creator("/path/to/file.ext")
+        assert repr(file) == "File('/path/to/file.ext')"
+
+        class SubFile(File):
+            pass
+
+        path2 = "/world/subfile/00000000-0000-0000-0000-000000000000.json"
+        a_file = SubFile(path2)
+        assert repr(a_file) == f"SubFile('{path2}')"
+
+
+class TestPlayerDataFile:
+    def test_inheritance(self):
+        player_data_file = PlayerDataFile("path")
+        assert isinstance(player_data_file, PlayerDataFile)
+        assert isinstance(player_data_file, File)
+
+
+class TestStatsFile:
+    def test_inheritance(self):
+        stats_file = StatsFile("path")
+        assert isinstance(stats_file, StatsFile)
+        assert isinstance(stats_file, File)
+
+
+class TestAdvancementsFile:
+    def test_inheritance(self):
+        advancements_file = AdvancementsFile("path")
+        assert isinstance(advancements_file, AdvancementsFile)
+        assert isinstance(advancements_file, File)
