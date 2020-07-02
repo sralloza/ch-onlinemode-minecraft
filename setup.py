411b62a2effe095ffa09c25573a1f39b85361c7b
sralloza <sralloza@gmail.com>
2020-07-02 17:28:59 +0200

    feat: use versioneer to manage versions

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@ from pathlib import Path
 
 from setuptools import find_packages, setup
 
+import versioneer
+
 requirements = Path(__file__).with_name("requirements.txt").read_text().splitlines()
 
 setup(
     name="server-manager",
-    version="1.0",
+    version=versioneer.get_version(),
     packages=find_packages(),
     install_requires=requirements,
+    cmdclass=versioneer.get_cmdclass(),
 )
