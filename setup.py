ce3d253c4c077ea4f6834ae96503ad2210bdfcae
sralloza <sralloza@gmail.com>
2020-07-07 20:18:52 +0200

    feat: add entry point

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ requirements = Path(__file__).with_name("requirements.txt").read_text().splitlin
 setup(
     name="server-manager",
     version=versioneer.get_version(),
+    entry_points={"console_scripts": ["lia=server_manager.main:main"]},
     packages=find_packages(),
     install_requires=requirements,
     cmdclass=versioneer.get_cmdclass(),
